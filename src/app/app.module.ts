998055ee72efc2f268e6995516712bb6a1226051
fdolzanes1 <fdolzanes1@gmail.com>
2022-01-28 13:35:16 -0400

    adicionando componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { ReplacePipe } from './pipe/replace.pipe';
+
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './components/courses/course-list.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 import { StarComponent } from './components/stars/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
